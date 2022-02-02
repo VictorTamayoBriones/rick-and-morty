69fc11cb319ade37b2f92471001873b457557eba
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-02-02 13:06:41 -0600

    theme

---
+++ src/theme.jsx
@@ -0,0 +1,14 @@
+export const theme = {
+    Blue400: '#3D9CF7',
+    Cyan800: '#368d93',
+    Cyan900: '#206c7a',
+    Green100: '#cffcd1',
+    Green200: '#a1f9ac',
+    Green700: '#0b905f',
+    Green800: '#077457',
+    Gray1: '#111',
+    Gray2: '#333',
+    white: '#f4f4f4',
+    Yellow300: '#fcd57a',
+    Yellow400: '#f9c258'
+}
